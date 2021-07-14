14f0a56db4520c6a10379575a4e837cb62163085
abmarie <77949813+abmarie@users.noreply.github.com>
2021-07-14 15:58:09 -0700

    updated indexing, error messaging, and bug fixes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 const gallery = document.querySelector(".gallery");
-
+let employeeData = "";
 //-------------------------------
 // FETCH FUNCTIONS
 //-------------------------------
@@ -8,22 +8,27 @@ const gallery = document.querySelector(".gallery");
 fetch("https://randomuser.me/api/?results=12&nat=us")
   .then(checkStatus)
   .then((results) => results.json())
-  .then((data) => createCard(data.results));
+  .then(function (res) {
+    employeeData = res.results;
+  })
+  .then(createCard);
 
 function checkStatus(response) {
   if (response.ok) {
     return Promise.resolve(response);
   } else {
+    let errorHTML = `<h2 class="error">${response.statusText}. Please try again later.</h2>`;
+    gallery.insertAdjacentHTML("beforeend", errorHTML);
     return Promise.reject(new Error(respose.statusText));
   }
 }
 //-------------------------------
 // EMPLOYEE CARD FUNCTIONS
 //-------------------------------
-function createCard(data) {
-  for (let i = 0; i < data.length; i++) {
-    let person = data[i];
-    let newCard = `<div class="card" id="${i}">
+function createCard() {
+  for (let i = 0; i < employeeData.length; i++) {
+    let person = employeeData[i];
+    let newCard = `<div class="card">
         <div class="card-img-container">
             <img class="card-img" src="${person.picture.large}" alt="profile picture">
         </div>
@@ -34,27 +39,36 @@ function createCard(data) {
         </div>
       </div>`;
     gallery.insertAdjacentHTML("beforeend", newCard);
+
+    //click event listner for card to bring up modal
     let card = gallery.lastElementChild;
-    card.addEventListener("click", (e) => findCard(e, data));
+    card.addEventListener("click", (e) => findCard(e));
   }
 }
 
-function findCard(event, data) {
+function findCard(event) {
+  //finds parent node of click event
   if (event.target.className === "card") {
-    let index = event.target.id;
+    let parent = event.target;
   } else if (event.target.parentNode.className === "card") {
-    index = event.target.parentNode.id;
+    parent = event.target.parentNode;
   } else if (event.target.parentNode.parentNode.className === "card") {
-    index = event.target.parentNode.parentNode.id;
+    parent = event.target.parentNode.parentNode;
   }
-  createModal(data, index);
+  //uses email to determine index in employeeData array
+  let infoCard = parent.lastElementChild;
+  let infoCardElements = infoCard.childNodes;
+  let email = infoCardElements[3].textContent;
+  let index = employeeData.findIndex((person) => person.email === email);
+  //passes clicked index to modal
+  createModal(index);
 }
 
 //-------------------------------
 // MODAL FUNCTIONS
 //-------------------------------
-function createModal(data, index) {
-  let current = data[index];
+function createModal(index) {
+  let current = employeeData[index];
   let loc = current.location;
   let modal = `
     <div class="modal-container">
@@ -85,7 +99,11 @@ function createModal(data, index) {
 
   //event listeners for toggle buttons
   let navigationButtons = document.querySelector(".modal-btn-container");
-  navigationButtons.addEventListener("click", (e) => navigate(e, data, index));
+  navigationButtons.addEventListener("click", function (e) {
+    if (e.target.type === "button") {
+      navigate(e, index);
+    }
+  });
 }
 
 //close modal using the 'x'
@@ -94,18 +112,18 @@ function hideModal() {
 }
 
 //modal toggle to navigate between employees when modal is open
-function navigate(e, data, index) {
+function navigate(e, index) {
   hideModal();
   switch (e.target.textContent) {
     case "Prev":
       if (index == 0) {
-        index = data.length - 1;
+        index = employeeData.length - 1;
       } else {
         index--;
       }
       break;
     case "Next":
-      if (index == data.length - 1) {
+      if (index == employeeData.length - 1) {
         index = 0;
       } else {
         index++;
@@ -114,7 +132,7 @@ function navigate(e, data, index) {
     default:
       hideModal();
   }
-  createModal(data, index);
+  createModal(index);
 }
 //-------------------------------
 // SEARCH TOOL FUNCTIONS
